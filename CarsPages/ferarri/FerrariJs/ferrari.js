b1f9003f112648d2be24734e5a04cd99381502a3
shohrux66 <u69kang.@gmail.com>
2023-09-25 06:22:27 -0700

    changed more events

--- CarsPages/ferarri/FerrariJs/ferrari.js
+++ CarsPages/ferarri/FerrariJs/ferrari.js
@@ -1,30 +1,34 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+// var myIndex = 0;
+// carousel();
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
+// function carousel() {
+//   var i;
+//   var x = document.querySelector(".in-slider");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {
+//     myIndex = 1;
+//   }
+//   x[myIndex - 1].style.display = "block";
+//   setTimeout(carousel, 5000); // Change image every 2 seconds
+// }
+let center_slider = document.querySelector(".medium-slider");
+let in_slider = document.querySelector(".in-slider");
+let the_index = 0;
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
+slider();
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
+function slider() {
+  var i;
+  for (i = 0; i < the_index.length; i++) {
+    the_index[i].syle.display = "none";
   }
-  if (n < 1) {
-    slideIndex = slides.length;
+  the_index++;
+  if (the_index > the_index.length) {
+    the_index = 1;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  the_index[the_index - 1].style.display = "block ";
+  setTimeout(slider, 10000);
 }
--- CarsPages/lambarghini/lamborghhini_js/lamborghini.js
+++ CarsPages/lambarghini/lamborghhini_js/lamborghini.js
@@ -1,11 +1,14 @@
 let cars = document.querySelector(".cars");
+let moreInformations = document.querySelector(".more-informations");
 
+
+// top contens
 async function getSmallApi(api) {
   try {
     const response = await fetch(api);
     const data = await response.json();
     if (response) {
-      showSmallContents(data);console.log(data);
+      showSmallContents(data);
     }
   } catch (err) {
     console.log(err.message, "Erorr");
@@ -39,5 +42,66 @@ function showSmallContents(carData) {
     </div>
     `;
     cars.innerHTML = theCarth;
+  });
+}
+
+// sider cars
+
+async function getMoreapi(apjsn) {
+  try {
+    const responsed = await fetch(apjsn);
+    const moreData = await responsed.json();
+
+    if (responsed) {
+      showMoreApiFnC(moreData);
+      console.log(moreData, "<===");
+    }
+  } catch (err) {
+    console.log(err.message, "Erorr");
+  }
+}
+
+getMoreapi("../lamborghhini_js/bigger.json");
+
+function showMoreApiFnC(moreCars) {
+  theCar = ``;
+  moreCars.forEach((mrCAR) => {
+    theCar += `
+    <div class="content">
+    <div class="content-vidio">
+      <video
+        src="${mrCAR.theCarVideo}"
+        controls
+        autoplay
+        muted
+      ></video>
+    </div>
+    <div class="big-description">
+      <div class="location">
+        <c>></c>
+        <o>
+          <h6>${mrCAR.theCarModelName}</h6>
+          <p>MODEL INFORMATIONS</p>
+        </o>
+      </div>
+      <div class="options-1">
+        <p>POWER (COMBINED ICE+EE)</p>
+        <h6>${mrCAR.theCarPowerCMB}</h6>
+      </div>
+      <div class="options-2">
+        <p>MAX.SPEED</p>
+        <p>${mrCAR.theCarMXSpeed}</p>
+      </div>
+      <div class="options-3">
+        <p>0-100 KM/H</p>
+        <p>${mrCAR.theCarSEC}</p>
+      </div>
+      <div class="bottom-descrip">
+        <h4>< Click on two marts for a better view</h5>
+      </div>
+    </div>
+  </div>
+    `;
+    moreInformations.innerHTML = theCar;
   });
 }
